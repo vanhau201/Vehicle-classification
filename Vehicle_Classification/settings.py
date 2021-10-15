8d553e21625b2b90a39ce30bf0638714ef8b96c6
VanHau2000 <86721832+VanHau2000@users.noreply.github.com>
2021-10-15 16:40:25 +0700

    update

--- Vehicle_Classification/settings.py
+++ Vehicle_Classification/settings.py
@@ -138,7 +138,7 @@ if DEBUG:
     os.path.join(BASE_DIR, 'static'),
 )
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, '/code/static')
+    STATIC_ROOT ='/code/static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
